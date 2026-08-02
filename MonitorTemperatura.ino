1fb49eefbea44af76411a7b6d94e8eda8a5d7cb2
AzNitro474 <azmartin474@gmail.com>
2026-08-01 20:32:01 -0500

    Lectura analógica compatible con sensores LM35 y TMP36

---
+++ MonitorTemperatura.ino
@@ -0,0 +1,13 @@
+int tempC;
+const int sensorTemp= A0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+tempC = analogRead(sensorTemp);
+Serial.println(tempC);
+}
